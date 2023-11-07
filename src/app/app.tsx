d908af5c9141756ecae891907b0e43cba81d810a
DimonKniaziev <dimkniazz@gmail.com>
2023-11-07 11:33:55 +0200

    add-pages-and-styles-to-app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,7 +2,11 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../components/header";
 import RandomPlanet from "../components/random-planet";
-import { LoginPage, SecretPage } from "../components/pages";
+import { LoginPage, PlanetsPage, SecretPage, StarshipsPage } from "../components/pages";
+import { PeoplePage } from "../components/pages";
+
+import "./app.scss"
+import MainPage from "../components/pages/main-page";
 
 const App: React.FC = () => {
 
@@ -11,15 +15,15 @@ const App: React.FC = () => {
     })
     
     return (
-        <div className="app">            
+        <div className="app">
             <Router>
                 <Header/>
                 <RandomPlanet />
                 <Routes>
-                    <Route path="/" element={<h1>Welcome to Star DB</h1>}/>
-                    {/* <Route path="/people" element={<PeoplePage/>}/>
+                    <Route path="/" element={<MainPage/>}/>
+                    <Route path="/people" element={<PeoplePage/>}/>
                     <Route path="/planets" element={<PlanetsPage/>}/>
-                    <Route path="/starships" element={<StarshipsPage/>}/> */}
+                    <Route path="/starships" element={<StarshipsPage/>}/>
                     <Route path="/secret" element={<SecretPage/>}/>
                     <Route path="/login" element={<LoginPage/>}/>
 
