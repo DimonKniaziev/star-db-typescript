9c58ab7d99c03a9659c67b66d490575cb1444de8
DimonKniaziev <dimkniazz@gmail.com>
2023-11-07 11:23:59 +0200

    adding-main-page

---
+++ src/components/pages/main-page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./main-page.scss"
+
+const MainPage: React.FC = () => {
+    return (
+        <div className="main-page">
+            <h4>
+                WECOME TO STAR-DB!
+            </h4>
+        </div>      
+    );
+};
+
+export default MainPage;
